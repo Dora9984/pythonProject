91c8d7c77d575c5af6bb8f2a9d603df90999bb28
Dora9984 <silbee08@gmail.com>
2023-08-31 08:36:38 +0900

    08/31

---
+++ section02/ex02-1-circle.py
@@ -0,0 +1,22 @@
+'''
+파일명: ex02-1-circle.py
+
+개요: 반지름을 전달하면 원의 넓이를 반환하는 get_area() gkatn
+'''
+
+# math 모듈 포함
+import math
+
+# get_area() 함수 정의
+def get_area(radius):
+    """반지름을 입력받아서 원의 넓이를 반환하는 get_area() 함수"""
+    are__) # Docstring 확인a = math.pi * math.pow(radius, 2) # 원의 넓이값 구하는 공식
+    return area
+
+# 실행 코드
+radius = 1.5 # 반지름 1.5
+
+# get_area() 함수 호출 결과를 area 변수에 저장
+area = get_area(radius)
+print(area)
+print(get_area.__doc__) # Docstring 확인
